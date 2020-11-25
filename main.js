5780876223c462fff0b164c95b81a3f7efd30cf1
Adam Rekosiewicz <rekosiewicz.adam@gmail.com>
2020-11-25 22:17:13 +0100

    misaligned tiles fix

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ function createWindow () {
     win = null
   })
 }
-
+app.commandLine.appendSwitch('--ignore-gpu-blacklist');
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
